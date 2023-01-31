65a0f08fbd2b511025f3d50ba29b99d932528be8
Kevin Lotz <kel@informatik.uni-kiel.de>
2023-01-31 10:04:23 +0100

    Support str.at

--- QF_S.thy
+++ QF_S.thy
@@ -5,6 +5,7 @@ begin
 
 abbreviation str_concat:: "'a word \<Rightarrow> 'a word  \<Rightarrow> 'a word"  where "(str_concat) u v \<equiv> u*v" 
 abbreviation str_len:: "'a word \<Rightarrow> nat" where  "str_len w \<equiv> size w"
+abbreviation str_at:: "'a word \<Rightarrow> nat \<Rightarrow> 'a word" where "str_at w i \<equiv> (case (at w i) of (option.Some a) \<Rightarrow> (Con a  Epsilon) | option.None \<Rightarrow> Epsilon)"
 
 abbreviation str_prefixof:: "'a word \<Rightarrow> 'a word \<Rightarrow> bool" where "str_prefixof \<equiv> Words.is_prefix"
 abbreviation str_suffixof:: "'a word \<Rightarrow> 'a word \<Rightarrow> bool" where "str_suffixof \<equiv> Words.is_suffix"
