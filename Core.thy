a8ac23a3951fd6894eec4f5242459644b5fd9524
Kevin Lotz <kel@informatik.uni-kiel.de>
2023-01-31 17:36:33 +0100

    Add simps for ITE expressions

--- Core.thy
+++ Core.thy
@@ -4,4 +4,12 @@ begin
 
 abbreviation ite:: "bool \<Rightarrow> 's \<Rightarrow> 's \<Rightarrow> 's" where "ite a B C  \<equiv> if a then B else C"
 
+lemma ite_true[simp]: "(ite a B C) \<and> a \<Longrightarrow> B"
+  apply(simp)
+  done
+
+lemma ite_false[simp]: "(ite a B C) \<and> \<not> a \<Longrightarrow> C"
+  apply(simp)
+  done
+
 end
