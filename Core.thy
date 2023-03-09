628de95cc50b40408f1221da8ecceee941eec2b4
Kevin Lotz <kel@informatik.uni-kiel.de>
2023-03-09 10:09:14 +0100

    Small enhancement

--- Core.thy
+++ Core.thy
@@ -2,12 +2,7 @@ theory Core
   imports Main
 begin
 
-definition ite:: "bool \<Rightarrow> 's \<Rightarrow> 's \<Rightarrow> 's" where "ite a B C  \<equiv> if a then B else C"
+fun ite:: "bool \<Rightarrow> 's \<Rightarrow> 's \<Rightarrow> 's" where "ite a B C = (if a then B else C)"
 
-lemma ite_true[simp]: "(ite True B C) \<Longrightarrow> B"
-  by (simp add: ite_def)
-
-lemma ite_false[simp]: "(ite False B C) \<Longrightarrow> C"
-  by (simp add: ite_def)
 
 end
